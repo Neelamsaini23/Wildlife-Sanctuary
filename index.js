05986e7f4552732c7c9f32470dc03fce956f4621
Neeam saini <neelamsaini23@navgurukul.org>
2024-06-17 11:57:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+    window.location.href = 'https://partyrock.aws/u/Bibhu/_zw8GxdmU/Indian-Wildlife-Sanctuary-Compendium';
+});
